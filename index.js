b7d93f706d038a71b8a424630efb1b524ad62931
RalucaZota <zotaraluca649@gmail.com>
2025-05-31 21:02:30 +0300

    added delete functionality

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const input = document.querySelector('input');
 const addButton = document.querySelector('.add-button');
-// const deleteButton = document.querySelector('.delete-button');
 const listBody = document.querySelector('ol');
-
+let counter = 0;
+let tasks = [];
+let task = { id: counter, value: '' };
 
 const addTask = () => {
-    if (input.value.trim() === '') return;
-    const taskElement = document.createElement('li')
-    const deleteTask = document.createElement('button')
-    deleteTask.textContent = 'Delete task'
-    taskElement.textContent = input.value
-    listBody.appendChild(taskElement)
-    taskElement.appendChild(deleteTask)
-    input.value = ''
-}
+  if (input.value.trim() === '') return;
+  let task = { id: counter++, value: input.value };
+  tasks.push(task);
 
+  const taskElement = document.createElement('li');
+  taskElement.setAttribute('id', task.id);
 
-addButton.addEventListener('click', addTask)
+  const taskText = document.createElement('span');
+  taskText.textContent = task.value;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete task';
+  console.log(task, 'task');
+
+  taskElement.appendChild(taskText);
+  listBody.appendChild(taskElement);
+  taskElement.appendChild(deleteButton);
+
+  input.value = '';
+
+  deleteButton.addEventListener('click', deleteTask);
+};
+
+const deleteTask = (event) => {
+  const li = event.target.parentElement;
+  const idToDelete = parseInt(li.id);
+  li.remove();
+
+  const filteredTasks = tasks.filter((task) => task.id !== idToDelete);
+  tasks = filteredTasks;
+  return tasks;
+};
+
+addButton.addEventListener('click', addTask);
